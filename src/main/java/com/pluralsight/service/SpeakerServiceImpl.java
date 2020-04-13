4ffa16f4ea039066808d0a266ee0c0066d0333f8
Dovénê AGOGUE <dov@MacBook-Pro-de-Sam.local>
2020-04-13 08:46:48 +0200

    using bean configuration

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -8,11 +8,24 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl(){
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository){
+        System.out.println("SpeakerServiceImpl repository constructor");
+        this.repository = speakerRepository;
+    }
 
     @Override
     public  List<Speaker> findAll(){
         return repository.findAll();
     }
 
+    public void setRepository(SpeakerRepository speakerRepository){
+        System.out.println("SpeakerServiceImpl repository setter");
+        this.repository = speakerRepository;
+    }
 }
