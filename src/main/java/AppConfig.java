3eba14372907cd3f7143975c4a70842e19c24c21
Dovénê AGOGUE <dov@MacBook-Pro-de-Sam.local>
2020-04-13 09:44:10 +0200

    added autowiring

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,12 +4,15 @@ import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
+    /*
     @Bean(name= "speakerService")
     @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
@@ -20,5 +23,5 @@ public class AppConfig {
     @Bean(name= "speakerRepository")
     public SpeakerRepository getSpeakerRepository() {
         return new HibernateSpeakerRepositoryImpl();
-    }
+    }*/
 }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,29 +3,20 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
+    @Autowired
     private SpeakerRepository repository;
-
-    public SpeakerServiceImpl(){
-        System.out.println("SpeakerServiceImpl no args constructor");
-    }
-
-    public SpeakerServiceImpl(SpeakerRepository speakerRepository){
-        System.out.println("SpeakerServiceImpl repository constructor");
-        this.repository = speakerRepository;
-    }
 
     @Override
     public  List<Speaker> findAll(){
         return repository.findAll();
     }
 
-    public void setRepository(SpeakerRepository speakerRepository){
-        System.out.println("SpeakerServiceImpl repository setter");
-        this.repository = speakerRepository;
-    }
 }
